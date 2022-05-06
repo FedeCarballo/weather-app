c93660ffbbb5237d7ab6afd2011e6a6f3c98e1ca
Fede Carballo <carballo523@gmail.com>
2022-05-06 11:26:39 -0300

    nth-update

--- weather-web-app/src/App.js
+++ weather-web-app/src/App.js
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import search from './assets/search.png'
 import './App.css'
 import video from './assets/video/wallpaper2.mp4'
 import {BsThermometerSun,BsFillCloudsFill, BsWater} from 'react-icons/bs'
 import {GoLocation} from 'react-icons/go'
-
-
-
+import Clock from './components/Clock'
 
 function App() {
   const [data, setdata] = useState({})
   const [location,setlocation] = useState('');
-  const [count, setcount] = useState('');
-  const [month,setmonth] = useState('');
-  
-  // const url=  `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=es&appid=9159d68dde8c59e8ba2817c591aae402&units=metric`
 
+  // const url=  `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=es&appid=9159d68dde8c59e8ba2817c591aae402&units=metric`
+  
   const url= `https://api.openweathermap.org/data/2.5/forecast?q=${location}&lang=es&appid=9159d68dde8c59e8ba2817c591aae402&units=metric`
 
   const searchLocation = (e) => {
@@ -28,23 +24,7 @@ function App() {
     )  
   setlocation('')
   }
-  const days=['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb' ]
 
-  const months = ['Ene' , 'Feb' , 'Mar' , 'Abr' , 'May' , 'Jun' , 'Jul' , 'Ago' , 'Sep' , 'Oct' , 'Nov' , 'Dic']
- 
-  useEffect(() => { setInterval(() => {
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const minute = time.getMinutes();
-    setcount(hour + ':' + minute + ' ')
-    setmonth(days[day]+'/'+date+'/'+months[month])
-  }, 1000);}, [])
-
-  
-console.log(count);
   return (
     <div className="App">
       <div className='Principal-container'>
@@ -84,12 +64,7 @@ console.log(count);
                   <button type='submit' value={'Buscar'} onClick={searchLocation}><img src={search} alt={data.name}/></button>
               </div>
               <div className='c5-subcontainer'>
-                <div>
-                 {count}
-                </div>
-                <div>
-                  {month}
-                </div>
+              <Clock />
                 <hr/>
               {data.list ? 
                 <div>
---
+++ weather-web-app/src/components/Clock.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Clock() {
+    const [count, setcount] = useState('');
+    const [month,setmonth] = useState('');
+
+    const days=['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb' ]
+  
+    const months = ['Ene' , 'Feb' , 'Mar' , 'Abr' , 'May' , 'Jun' , 'Jul' , 'Ago' , 'Sep' , 'Oct' , 'Nov' , 'Dic']
+   
+    useEffect(() => { setInterval(() => {
+      const time = new Date();
+      const month = time.getMonth();
+      const date = time.getDate();
+      const day = time.getDay();
+      const hour = time.getHours();
+      const minute = time.getMinutes();
+      setcount(hour + ':' + minute + ' ')
+      setmonth(days[day]+'/'+date+'/'+months[month])
+    }, 1000);}, [])
+  
+  return (
+    <div>
+        <div>
+            {count}
+        </div>
+        <div>
+            {month}
+        </div>
+    </div>
+  )
+}
